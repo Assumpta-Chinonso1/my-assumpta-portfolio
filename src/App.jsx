e21a291ab4dd030efc9f843c12877d178862932c
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-31 19:28:43 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
-import About from './Components/About'
+
 
 const App = () => {
   return (
