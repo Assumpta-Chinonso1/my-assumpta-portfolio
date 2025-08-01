73451bd9a04531a9d11acafb645fc53d26e016a0
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-08-01 15:17:47 +0100

    new commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import useInViewFade from './hooks/useInViewFade'
+import profileImg from '../assets/profile-1.png'
 
+const About = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default About
